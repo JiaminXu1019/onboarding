6322b7aacde873b618cac485e0c05b1c60fc0535
Jiamin Xu <jiaminxu@Jiamins-MacBook-Pro.local>
2020-11-12 22:10:55 -0800

    formatting

--- src/Post.js
+++ src/Post.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Post.css';
 
 export default function Post({ title, author, body }) {
   return (
     <>
-      <div>
-        Title:
-        {title}
-      </div>
-      <div>
-        Author:
-        {author}
-      </div>
-      <div>
-        Body:
-        {body}
+      <div className="bigdiv">
+        <div className="header">Title:</div>
+        <div className="div">
+          {title}
+        </div>
+        <div className="header">Author:</div>
+        <div className="div">
+          {author}
+        </div>
+        <div className="header">Body:</div>
+        <div className="div">
+          {body}
+        </div>
       </div>
     </>
   );
--- src/PostInput.js
+++ src/PostInput.js
@@ -14,10 +14,31 @@ export default function PostInput() {
 
   return (
     <>
-      <input onChange={(e) => setPost({ ...post, title: e.target.value })} />
-      <input onChange={(e) => setPost({ ...post, body: e.target.value })} />
-      <input onChange={(e) => setPost({ ...post, author: e.target.value })} />
-      <button type="button" onClick={() => { addToFirebase(); }}>Submit</button>
+      <div className="bigdiv2">
+        New Post
+        <div className="header">
+          Title
+        </div>
+        <div>
+          <input className="div" onChange={(e) => setPost({ ...post, title: e.target.value })} />
+        </div>
+        <div className="header">
+          Body
+        </div>
+        <div>
+          <input className="div" onChange={(e) => setPost({ ...post, body: e.target.value })} />
+        </div>
+        <div className="header">
+          Author
+        </div>
+        <div>
+          <input className="div" onChange={(e) => setPost({ ...post, author: e.target.value })} />
+        </div>
+        <div>
+          <button type="button" onClick={() => { addToFirebase(); }}>Submit</button>
+        </div>
+      </div>
+
     </>
   );
 }
